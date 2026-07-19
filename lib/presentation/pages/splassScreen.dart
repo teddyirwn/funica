73b255bf546756fc2038b95ac30614ba6a4f4fca
FaithEndrian <muhammadfaith14506@gmail.com>
2026-07-19 14:43:15 +0700

    menambah splass screen

---
+++ lib/presentation/pages/splassScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splassscreen extends StatefulWidget {
+  const Splassscreen({super.key});
+
+  @override
+  State<Splassscreen> createState() => _SplassscreenState();
+}
+
+class _SplassscreenState extends State<Splassscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
